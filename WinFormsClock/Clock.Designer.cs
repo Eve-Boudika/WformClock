ef7339bdd0e84e070fbad8d0b62d9e06fb14b3c5
Evelyn <evelabohemia@gmail.com>
2022-12-17 20:52:08 -0300

    resize

--- WinFormsClock/Clock.Designer.cs
+++ WinFormsClock/Clock.Designer.cs
@@ -42,11 +42,11 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("DS-Digital", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Font = new System.Drawing.Font("DS-Digital", 39.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.label1.Location = new System.Drawing.Point(45, 64);
+            this.label1.Location = new System.Drawing.Point(12, 28);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(223, 63);
+            this.label1.Size = new System.Drawing.Size(178, 52);
             this.label1.TabIndex = 0;
             this.label1.Text = "19:38:12";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -61,7 +61,7 @@
             this.Argentina.AutoSize = true;
             this.Argentina.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.Argentina.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.Argentina.Location = new System.Drawing.Point(45, 34);
+            this.Argentina.Location = new System.Drawing.Point(12, 9);
             this.Argentina.Name = "Argentina";
             this.Argentina.Size = new System.Drawing.Size(83, 19);
             this.Argentina.TabIndex = 1;
@@ -70,11 +70,11 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("DS-Digital", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Font = new System.Drawing.Font("DS-Digital", 39.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.label2.Location = new System.Drawing.Point(45, 169);
+            this.label2.Location = new System.Drawing.Point(12, 99);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(223, 63);
+            this.label2.Size = new System.Drawing.Size(178, 52);
             this.label2.TabIndex = 2;
             this.label2.Text = "19:38:12";
             // 
@@ -83,7 +83,7 @@
             this.Mexico.AutoSize = true;
             this.Mexico.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.Mexico.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.Mexico.Location = new System.Drawing.Point(45, 140);
+            this.Mexico.Location = new System.Drawing.Point(12, 80);
             this.Mexico.Name = "Mexico";
             this.Mexico.Size = new System.Drawing.Size(63, 19);
             this.Mexico.TabIndex = 3;
@@ -95,7 +95,7 @@
             this.Peru.AutoSize = true;
             this.Peru.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.Peru.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.Peru.Location = new System.Drawing.Point(45, 246);
+            this.Peru.Location = new System.Drawing.Point(12, 151);
             this.Peru.Name = "Peru";
             this.Peru.Size = new System.Drawing.Size(135, 19);
             this.Peru.TabIndex = 4;
@@ -104,11 +104,11 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("DS-Digital", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Font = new System.Drawing.Font("DS-Digital", 39.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.label3.Location = new System.Drawing.Point(45, 277);
+            this.label3.Location = new System.Drawing.Point(12, 170);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(223, 63);
+            this.label3.Size = new System.Drawing.Size(178, 52);
             this.label3.TabIndex = 5;
             this.label3.Text = "19:38:12";
             this.label3.Click += new System.EventHandler(this.label3_Click_1);
@@ -118,7 +118,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(312, 396);
+            this.ClientSize = new System.Drawing.Size(242, 233);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.Peru);
             this.Controls.Add(this.Mexico);
@@ -130,7 +130,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Clock";
-            this.Opacity = 0.75D;
+            this.Opacity = 0.8D;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Reloj";
             this.Load += new System.EventHandler(this.Clock_Load);
